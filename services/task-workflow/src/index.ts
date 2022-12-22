db62f58cf2aa3b83adf13017c6ff1a1e6347f6c7
Yasith Ariyasena <yasith.ariyasena@gmail.com>
2022-12-22 21:37:27 +0530

    refactor: Move Apollo server creation and setup to a separate module

--- services/task-workflow/src/index.ts
+++ services/task-workflow/src/index.ts
@@ -1,40 +1,18 @@
-import { ApolloServer } from '@apollo/server';
 import { ValidateJwt, PerformAuthorization } from '@task-workflow/user-auth';
 import express from 'express';
-import { taskQueryResolvers } from './domains/task';
-import { taskTypeDefs } from './graphql/schema';
-import { getTasks } from './route-handlers';
-import { json } from 'body-parser';
-import cors from 'cors';
-import { expressMiddleware } from '@apollo/server/express4';
 import http from 'http';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { setupGraphQLEndpoint, startApolloServer } from './graphql';
 
 async function init() {
     const app = express();
     const port = 3001;
     const httpServer = http.createServer(app);
 
-    const apolloServer = new ApolloServer({
-        typeDefs: taskTypeDefs,
-        resolvers: {
-            ...taskQueryResolvers,
-        },
-        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    });
-    await apolloServer.start();
+    const apolloServer = await startApolloServer(httpServer);
 
     app.use(ValidateJwt('http://localhost:3000'));
 
-    app.get('/tasks', PerformAuthorization(['ADMIN']), getTasks);
-
-    app.use(
-        '/graphql',
-        cors<cors.CorsRequest>(),
-        json(),
-        //PerformAuthorization(['END_USER']),
-        expressMiddleware(apolloServer)
-    );
+    setupGraphQLEndpoint(app, apolloServer);
 
     httpServer.listen(port, () => {
         console.log(`Task Workflow Service listening on port ${port}`);
